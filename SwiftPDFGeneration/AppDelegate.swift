c1d7b91c18dd3c77a8844856462fd1c9c7fd4d94
Debasis Das <debasisdas@debasiss-mac-mini.local>
2016-02-01 00:05:23 +0530

    Initial Commit

---
+++ SwiftPDFGeneration/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SwiftPDFGeneration
+//
+//  Created by Debasis Das on 01/02/16.
+//  Copyright © 2016 Knowstack. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
